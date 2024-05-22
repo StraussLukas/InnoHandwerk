a97f20bbf4b63aeb8c459cb0df8b54b4e73a1790
lukas <lukas.strauss1404@gmail.com>
2024-05-22 19:28:09 +0200

    Basisproject erstellt. Dependencies eingefügt: - h2 als Datenbank -> in File Datenbank, somit automatisch im Repo gespeichert - jpa für Datenbankzugriff - flyway als Migrations-/Versionierungstool - lombok als zusätzliche Java libary - Spring boot starter Web für REST Schnittstelle - Spring boot starter test für tests
    Configuartionen getroffen
    - application.yml -> Basisconfiguration
    - application-integrationtest.yml -> Integrationtestconfiguration
    - application-jpa-test.yml -> jpa-test-configuration

---
+++ backend/src/main/java/com/example/InnoHandwerk/InnoHandwerk/InnoHandwerkApplication.java
@@ -0,0 +1,13 @@
+package com.example.InnoHandwerk.InnoHandwerk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnoHandwerkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InnoHandwerkApplication.class, args);
+	}
+
+}
