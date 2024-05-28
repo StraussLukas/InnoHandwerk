ef20becb86b569e5774886f496a8429d1b0958bc
sebith <sebastian.thoenes@edu.fhdw.de>
2024-05-28 10:32:29 +0200

    Ordnerstruktur und initialen Komponenten erstellen

---
+++ frontend/src/app/components/shared/project-info-tile/project-info-tile.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-project-info-tile',
+  standalone: true,
+  imports: [],
+  templateUrl: './project-info-tile.component.html',
+  styleUrl: './project-info-tile.component.css'
+})
+export class ProjectInfoTileComponent {
+
+}
