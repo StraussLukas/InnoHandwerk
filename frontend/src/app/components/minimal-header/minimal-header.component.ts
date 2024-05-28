37db4e3adcfaf4bf0d3aefcf90f273cbd5ff4aaf
sebith <sebastian.thoenes@edu.fhdw.de>
2024-05-28 11:54:22 +0200

    Routing | Header

--- frontend/src/app/components/minimal-header/minimal-header.component.ts
+++ frontend/src/app/components/minimal-header/minimal-header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-minimal-header',
@@ -8,5 +9,9 @@ import { Component } from '@angular/core';
   styleUrl: './minimal-header.component.css'
 })
 export class MinimalHeaderComponent {
+
+  constructor() {
+
+  }
 
 }
