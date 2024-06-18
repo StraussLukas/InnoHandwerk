d8decec5126579fe884f0fa096a71b0add87618e
JustusStein <jutus_paul.stein@edu.fhdw.de>
2024-06-18 17:23:03 +0200

    edited test

--- backend/src/test/java/com/example/InnoHandwerk/InnoHandwerk/service/BaustellenbesetzungServiceTest.java
+++ backend/src/test/java/com/example/InnoHandwerk/InnoHandwerk/service/BaustellenbesetzungServiceTest.java
@@ -1,6 +1,8 @@
 package com.example.InnoHandwerk.InnoHandwerk.service;
 
+import com.example.InnoHandwerk.InnoHandwerk.entity.Baustelle;
 import com.example.InnoHandwerk.InnoHandwerk.entity.Baustellenbesetzung;
+import com.example.InnoHandwerk.InnoHandwerk.entity.Mitarbeiter;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -8,6 +10,7 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 
 import java.sql.Time;
+import java.sql.Timestamp;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -22,27 +25,81 @@ public class BaustellenbesetzungServiceTest {
     @Autowired
     private BaustellenBesetzungService service;
 
+    @Autowired
+    private  BaustelleService baustelleService;
+
+    @Autowired
+    private MitarbeiterService mitarbeiterService;
+
     private final Baustellenbesetzung besetzung1 = new Baustellenbesetzung();
     private final Baustellenbesetzung besetzung2 = new Baustellenbesetzung();
     private final Baustellenbesetzung besetzung3 = new Baustellenbesetzung();
 
+    private final Baustelle baustelle = new Baustelle();
+    private final Baustelle baustelle2 = new Baustelle();
+
+
+    private final Mitarbeiter mitarbeiter1001 = new Mitarbeiter();
+    private final Mitarbeiter mitarbeiter1002 = new Mitarbeiter();
 
     @BeforeAll
     void setUp() {
+        mitarbeiter1001.setPersonalnummer(1001);
+        mitarbeiter1001.setVorname("Max");
+        mitarbeiter1001.setNachname("Meier");
+        mitarbeiter1001.setEmail("maxmeier@email.com");
+        mitarbeiter1001.setAdmin(true);
+
+        mitarbeiter1002.setPersonalnummer(1002);
+        mitarbeiter1002.setVorname("Maria");
+        mitarbeiter1002.setNachname("Meier");
+        mitarbeiter1002.setEmail("mariameier@email.com");
+        mitarbeiter1002.setAdmin(false);
+
+        mitarbeiterService.addMitarbeiter(mitarbeiter1001);
+        mitarbeiterService.addMitarbeiter(mitarbeiter1002);
+
+        baustelle.setId(-1);
+        baustelle.setTitel("Baustelle 1");
+        baustelle.setName_bauherr("Bauherr1");
+        baustelle.setAdresse("Adresse1");
+        baustelle.setStatus("Erstellt");
+        baustelle.setTelefon("123456789");
+        baustelle.setEmail("bauherr1@example.com");
+        baustelle.setArbeitsaufwand(10);
+        baustelle.setZeitstempel(Timestamp.valueOf("2024-03-21 09:15:45"));
+
+
+        baustelleService.addBaustelle(baustelle);
+
+        baustelle2.setId(-2);
+        baustelle2.setTitel("Baustelle 1");
+        baustelle2.setName_bauherr("Bauherr1");
+        baustelle2.setAdresse("Adresse1");
+        baustelle2.setStatus("Erstellt");
+        baustelle2.setTelefon("123456789");
+        baustelle2.setEmail("bauherr1@example.com");
+        baustelle2.setArbeitsaufwand(10);
+        baustelle2.setZeitstempel(Timestamp.valueOf("2024-03-21 09:15:45"));
+
+
+        baustelleService.addBaustelle(baustelle2);
+
+
         besetzung1.setPersonalnummer(1001);
-        besetzung1.setBaustellenId(5);
+        besetzung1.setBaustellenId(-1);
         besetzung1.setDatum(20230530.0);
         besetzung1.setUhrzeitVon(Time.valueOf("08:00:00"));
         besetzung1.setUhrzeitBis(Time.valueOf("16:00:00"));
 
         besetzung2.setPersonalnummer(1002);
-        besetzung2.setBaustellenId(6);
+        besetzung2.setBaustellenId(-2);
         besetzung2.setDatum(20230530.0);
         besetzung2.setUhrzeitVon(Time.valueOf("09:00:00"));
         besetzung2.setUhrzeitBis(Time.valueOf("17:00:00"));
 
         besetzung3.setPersonalnummer(1001);
-        besetzung3.setBaustellenId(6);
+        besetzung3.setBaustellenId(-2);
         besetzung3.setDatum(20230530.0);
         besetzung3.setUhrzeitVon(Time.valueOf("08:00:00"));
         besetzung3.setUhrzeitBis(Time.valueOf("16:00:00"));
@@ -93,7 +150,7 @@ public class BaustellenbesetzungServiceTest {
         Optional<Baustellenbesetzung> actualEntity = service.getBaustellenBesetzungById(5);
         // assert
         assertThat(actualEntity).isPresent();
-        assertEquals(5, actualEntity.get().getBaustellenId());
+        assertEquals(-1, actualEntity.get().getBaustellenId());
     }
 
     @Order(5)
@@ -116,14 +173,14 @@ public class BaustellenbesetzungServiceTest {
     @Test
     void updateBaustellenBesetzung_whenValidModel_thenReturnEntityId() {
         // arrange
-        besetzung1.setBaustellenId(7);
+        besetzung1.setBaustellenId(-2);
         // actual
         var actualId = service.updateBaustellenBesetzung(besetzung1);
         var actualEntity = service.getBaustellenBesetzungById(5).orElse(null);
         // assert
         assertEquals(5, actualId);
         assertThat(actualEntity).isNotNull();
-        assertEquals(7, actualEntity.getBaustellenId());
+        assertEquals(-2, actualEntity.getBaustellenId());
     }
 
     @Order(8)
@@ -139,9 +196,13 @@ public class BaustellenbesetzungServiceTest {
     @Order(9)
     @Test
     @Sql(statements = {
-            "DELETE FROM baustellenbesetzung WHERE id = 5",
-            "DELETE FROM baustellenbesetzung WHERE id = 6",
-            "DELETE FROM baustellenbesetzung WHERE id = 7",
+            "DELETE FROM baustellenbesetzung WHERE id = '5'",
+            "DELETE FROM baustellenbesetzung WHERE id = '6'",
+            "DELETE FROM baustellenbesetzung WHERE id = '7'",
+            "DELETE FROM baustelle WHERE id = '-1'",
+            "DELETE FROM baustelle WHERE id = '-2'",
+            "DELETE FROM mitarbeiter WHERE personalnummer = '1001'",
+            "DELETE FROM mitarbeiter WHERE personalnummer = '1002'",
             "ALTER SEQUENCE baustellenbesetzung_id_seq RESTART"
     })
     void getAllBaustellenBesetzung_checkNumberOfEntitiesAfterDeletingTestData_mustBe1() {
